352f31c5d5c8b1f40f136e9bd691a017bdfe6231
Mayela1 <90623860+Mayela1@users.noreply.github.com>
2021-09-15 10:16:01 -0500

    paso 4 y 5

---
+++ practica05/src/mx/com/softgame/www/poo1game/personajes/Personaje.java
@@ -0,0 +1,33 @@
+package mx.com.softgame.www.poo1game.personajes;
+public class Personaje{
+	private String nombre;
+	private int edad;
+
+	public Personaje(String nombre){
+		edad = 0;
+		this.nombre = nombre;
+	}
+
+	public void setNombre(String nombre){
+	    this.nombre = nombre;
+	}
+
+	public String getNombre(){
+	    return nombre;
+	}
+
+	public void saludar(){
+		System.out.println("Hola Alumno de POO " + nombre);
+	}
+
+	public int getEdad(){
+		return edad;
+	}
+
+	public boolean setEdad(int edad){
+		if(edad > 0 && edad <120){
+			return true;
+		}
+		return false;
+	}
+}
