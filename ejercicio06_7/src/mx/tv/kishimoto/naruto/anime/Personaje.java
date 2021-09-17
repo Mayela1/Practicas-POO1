83e8be6b4c70a8b6a082bd26bdeca0e0f19c5061
Mayela1 <90623860+Mayela1@users.noreply.github.com>
2021-09-17 09:59:13 -0500

    paso 6

--- ejercicio06_7/src/mx/tv/kishimoto/naruto/anime/Personaje.java
+++ ejercicio06_7/src/mx/tv/kishimoto/naruto/anime/Personaje.java
@@ -1,3 +1,25 @@
 package mx.tv.kishimoto.anime;
 public class Personaje{
+	private String nombre;
+	private int poder;
+
+	public Personaje(String nombre, int valoresPoder){
+		this.nombre=nombre;
+		if(valoresPoder>=0 && valoresPoder<=500){
+			this.poder=valoresPoder;
+		}else{
+			this.poder=0
+		
+		}
+	}
+	public void setPoder(int sumaPoder){
+		if (valoresPoder>=0 && valoresPoder<=500){
+			this.poder += sumaPoder;
+		}
+	}
+
+	public void getDatos(){
+		System.out.println(nombre + "\t" + poder);
+
+	}
 }
