e58b864c49270bb2af43f2ad09c12afc0dd018c2
Mayela1 <90623860+Mayela1@users.noreply.github.com>
2021-10-08 08:10:36 -0500

    ejercicio06_7

--- ejercicio06_7/src/mx/tv/kishimoto/naruto/anime/Personaje.java
+++ ejercicio06_7/src/mx/tv/kishimoto/naruto/anime/Personaje.java
@@ -1,22 +1,20 @@
-package mx.tv.kishimoto.anime;
+package mx.tv.kishimoto.naruto.anime;
 public class Personaje{
 	private String nombre;
 	private int poder;
-
-	public Personaje(String nombre, int valoresPoder){
+	public Personaje(String nombre, int poder){
 		this.nombre=nombre;
-		if(valoresPoder>=0 && valoresPoder<=500){
-			this.poder=valoresPoder;
+		if(poder>=0 && poder<=500){
+			this.poder=poder;
 		}else{
-			this.poder=0
+			this.poder=0;
 		}
 	}
 	public void setPoder(int sumaPoder){
-		if (valoresPoder>=0 && valoresPoder<=500){
-			this.poder += sumaPoder;
+		if (poder>=0 && poder<=500){
+			poder += sumaPoder;
 		}
 	}
-
 	public String getDatos(){
 		return (nombre + "\t" + poder);
 	}
--- ejercicio06_7/src/mx/tv/kishimoto/naruto/pruebas/Escena.java
+++ ejercicio06_7/src/mx/tv/kishimoto/naruto/pruebas/Escena.java
@@ -1,25 +1,24 @@
 package mx.tv.kishimoto.naruto.pruebas;
-import mx.tv.kishimoto.anime.Personaje;
+import mx.tv.kishimoto.naruto.anime.Personaje;
 public class Escena{
 	public static void main(String[] args) {
-		Personaje naruto = new Personje("Naruto", 1000);
+		Personaje naruto = new Personaje("Naruto", 1000);
 		Personaje sasuke = new Personaje ("Sasuke", -10);
 		Personaje sakura = new Personaje("Sakura", 300);
-
 		System.out.println(naruto.getDatos());
 		System.out.println(sasuke.getDatos());
 		System.out.println(sakura.getDatos());
 		System.out.println("_ _ _");
-
-		Personaje usumaki = naruto
+		Personaje usumaki = naruto;
 		System.out.println(usumaki.getDatos());
 		System.out.println("_ _ _");
-
-		int n = (int)Math.round(Math.random()*1000)
+		int n;
+		n= (int)(Math.random()*1000);
 		while (n>500){
-			System.out.println(n)
-			int n = (int)Math.round(Math.random()*1000)
+			System.out.println(n);
+			n = (int)(Math.random()*1000);
 		}
-
+		usumaki.setPoder(n);
+		System.out.println(naruto.getDatos());
 	}
 }
