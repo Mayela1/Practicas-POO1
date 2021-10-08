eb3925f1dd60e60d43c08c41b3f99d8099739db3
Mayela1 <90623860+Mayela1@users.noreply.github.com>
2021-10-08 09:37:30 -0500

    Update Universitario.java

--- practica09/src/edu/uaz/ingsoft/appcovid/universitarios/Universitario.java
+++ practica09/src/edu/uaz/ingsoft/appcovid/universitarios/Universitario.java
@@ -4,4 +4,13 @@ public class Universitario{
 	public Universitario(String nombre){
 		this.nombre = nombre;
 	}
+	public boolean equals(Object o){
+		boolean result = false;
+		if ( (o != null) && (o instanceof Universitario)){
+			Universitario d =(Universitario) o;
+			if (this.nombre.equals()){
+				result = true;
+			}
+		}
+	}
 }
